7358e115bb476864968344b0e483e848b137fe6b
Cool-Boys <329991137@qq.com>
2017-05-03 22:26:20 +0800

    Signed-off-by: Cool-Boys <329991137@qq.com>

---
+++ page/my/pages/index/index.js
@@ -0,0 +1,138 @@
+Page({
+  data: {
+    message: '列表渲染',
+    array: [1, 2, 3, 4, 5],
+    message1: '条件渲染',
+    view: 'MINA',
+    reurnData: '没有数据',
+    staffA: { firstName: 'Hulk', lastName: 'Hu' },
+    staffB: { firstName: 'Shang', lastName: 'You' },
+    staffC: { firstName: 'Gideon', lastName: 'Lin' },
+    objectArray: [
+      { id: 5, unique: 'unique_5' },
+      { id: 4, unique: 'unique_4' },
+      { id: 3, unique: 'unique_3' },
+      { id: 2, unique: 'unique_2' },
+      { id: 1, unique: 'unique_1' },
+      { id: 0, unique: 'unique_0' },
+    ],
+    numberArray: [1, 2, 3, 4],
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+
+
+
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载
+    console.log('onload');
+  },
+  onReady: function () {
+    // 生命周期函数--监听页面初次渲染完成
+    console.log('onReady');
+  },
+  onShow: function () {
+    // 生命周期函数--监听页面显示
+    console.log('onShow');
+
+  },
+  onHide: function () {
+    // 生命周期函数--监听页面隐藏
+    console.log('onHide');
+  },
+  onUnload: function () {
+    // 生命周期函数--监听页面卸载
+    console.log('onUnload');
+  },
+  onPullDownRefresh: function () {
+    // 页面相关事件处理函数--监听用户下拉动作
+    console.log('onPullDownRefresh');
+  },
+  onReachBottom: function () {
+    // 页面上拉触底事件的处理函数
+    console.log('onReachBottom');
+  },
+  onShareAppMessage: function () {
+    // 用户点击右上角分享
+    return {
+      title: '自定义分享', // 分享标题
+      desc: '这是一个分享描述', // 分享描述
+      path: 'myInfo' // 分享路径
+    }
+  },
+  switch: function (e) {
+    const length = this.data.objectArray.length
+    for (let i = 0; i < length; ++i) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
+    }
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addToFront: function (e) {
+    const length = this.data.objectArray.length
+    this.data.objectArray.push({ id: length, unique: 'unique_' + length })
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+    console.log("添加事件" + length);
+  },
+  addNumberToFront: function (e) {
+    this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
+    })
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  getData: function (e) {
+
+    var that = this
+    wx.request({
+      url: 'http://localhost:14245/FLWebService.asmx/HelloWorld',
+      method: 'POST',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          reurnData: res.data.d
+
+        })
+      }
+    })
+
+  }
+
+})
---
+++ page/my/pages/shopcar/shopcar.js
@@ -0,0 +1,148 @@
+Page({
+    data: {
+        shopdata: [
+            { id: 1, name: '饭包', memo: '', saucetype: '鸡蛋酱+豆瓣酱', imageUrl: '../../../../image/fb.jpg', price: 6 },
+            { id: 2, name: '饭包', memo: '', saucetype: '', imageUrl: '../../../../image/fb.jpg', price: 6 },
+            { id: 3, name: '冷面', memo: '加烤肠', saucetype: '', imageUrl: '../../../../image/fb.jpg', price: 6 },
+            { id: 4, name: '饭包', memo: '', saucetype: '', imageUrl: '../../../../image/fb.jpg', price: 6 },
+            { id: 5, name: '饭包', memo: '', saucetype: '', imageUrl: '../../../../image/fb.jpg', price: 6 },
+            { id: 6, name: '饭包', memo: '', saucetype: '', imageUrl: '../../../../image/fb.jpg', price: 6 },
+            { id: 7, name: '饭包', memo: '', saucetype: '', imageUrl: '../../../../image/fb.jpg', price: 6 },
+        ],
+        flavortype: [
+            { name: '葱', flavortype: '1', ischeck: false },
+            { name: '辣', flavortype: '2', ischeck: false },
+            { name: '香菜', flavortype: '3', ischeck: false },
+            { name: '土豆丝', flavortype: '4', ischeck: false },
+            { name: '黄瓜丝', flavortype: '5', ischeck: false }
+        ],
+        saucetype: [
+            { name: '鸡蛋酱', ischeck: false }, { name: '豆瓣酱', ischeck: false }, { name: '辣酱', ischeck: false }
+        ],
+        curNav: 1,
+        curIndex: 0,
+        time: '12:01',
+        message: ''
+    },
+    switchMemoInput: function (e) {
+        // 获取item项的id，和数组的下标值  
+        let id = e.target.dataset.id,
+            index = parseInt(e.target.dataset.index);
+        console.log('id' + id + '  index:' + index);
+
+        var tt = [
+            { name: '鸡蛋酱', ischeck: false }, { name: '豆瓣酱', ischeck: false }, { name: '辣酱', ischeck: false }]
+        var dd = [
+            { name: '葱', flavortype: '1', ischeck: false },
+            { name: '辣', flavortype: '2', ischeck: false },
+            { name: '香菜', flavortype: '3', ischeck: false },
+            { name: '土豆丝', flavortype: '4', ischeck: false },
+            { name: '黄瓜丝', flavortype: '5', ischeck: false }
+        ]
+        // 把点击到的某一项，设为当前index  
+        // 把点击到的某一项，设为当前index  
+        this.setData({
+            curNav: id,
+            curIndex: index,
+            saucetype: tt,
+            flavortype:dd
+        })
+    },
+
+    radioChange: function (e) {
+        let text = e.detail.value;
+        let index = e.target.dataset.index;
+        let seltext = e.target.dataset.text;
+        //获取备注信息
+        let str = this.data.shopdata[this.data.curIndex].memo;
+        let strArr = str.split(" ");
+        var isNew = true;
+        for (var i = 0; i < strArr.length; i++) {
+            if (strArr[i].indexOf(seltext) > -1) {
+                strArr[i] = text;
+                isNew = false;
+            }
+
+        }
+        if (isNew) {
+            str += ' ' + text;
+        }
+        else {
+            str = strArr.toString().replace(/,/g, " ");
+        }
+        console.log('radio发生change事件，携带value值为：', text, index, seltext);
+        this.data.shopdata[this.data.curIndex].memo = str;
+
+        this.setData(
+            { shopdata: this.data.shopdata }
+        )
+    },
+    checkboxChange: function (e) {
+        console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+        let text = e.detail.value;
+        this.data.shopdata[this.data.curIndex].saucetype = text.toString().replace(/,/g, "+");
+
+        this.setData(
+            { shopdata: this.data.shopdata }
+        )
+    },
+    deletedata: function (e) {
+        let index = e.target.dataset.index;
+        console.log('deletedata事件，携带value值为：', index)
+        this.data.shopdata.splice(index, 1);
+        this.setData(
+            { shopdata: this.data.shopdata }
+        )
+    },
+    adddata: function (e) {
+        let index = this.data.curIndex;
+        let length = this.data.shopdata.length;
+        console.log('length值为：', length)
+        this.data.shopdata.splice(length + 1, 0,
+            { id: '', name: '饭包', memo: '', imageUrl: '../../../../image/fb.jpg' });
+        this.setData(
+            { shopdata: this.data.shopdata }
+        )
+    },
+    bindKeyInput: function (e) {
+        let text = e.detail.value;
+        this.data.shopdata[this.data.curIndex].memo = text;
+
+    },
+    bindblurInput: function (e) {
+        this.setData(
+            { shopdata: this.data.shopdata }
+        )
+    },
+    bindTimeChange: function (e) {
+        this.setData({
+            time: e.detail.value
+        })
+    },
+    bindMessageInput: function (e) {
+        let text = e.detail.value;
+        this.data.message = text;
+        console.log('message值为：', text)
+        this.setData(
+            { message: this.data.message }
+        )
+    },
+    bindSubmit: function () {
+        console.log('商品共计：' + this.data.shopdata.length + '个');
+        let str = '';
+        let amount = 0;
+        for (var i = 0; i < this.data.shopdata.length; i++) {
+            str += this.data.shopdata[i].name + " " + this.data.shopdata[i].memo;
+            amount += this.data.shopdata[i].price;
+        }
+
+        console.log('商品综述:' + str + " 金额共计：" + amount);
+
+        console.log('预定时间：' + this.data.time);
+        console.log('备注：' + this.data.message);
+        wx.switchTab({
+            url: '/page/my/pages/myInfo/myInfo'
+        })
+
+    }
+});
